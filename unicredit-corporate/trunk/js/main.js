6e060b8138c2c63778c893bde9914d3d80eb6fb5
gmacrimasi <gmacrimasi@70242730-a0bf-483c-8687-6f3b35fb5193>
2015-01-14 16:24:43 +0000

    Footer

--- unicredit-corporate/trunk/js/main.js
+++ unicredit-corporate/trunk/js/main.js
@@ -1,26 +1,39 @@
+var collapsedSidebar=80;
+var openedSidebar=280;
+var pixelSpeed=500/200;
+
+function getLeft(){
+	return $('#sidebar').hasClass('opened') ? openedSidebar : collapsedSidebar;
+}
+function getContentHeight(element){
+	var sum = 0;
+	$(element).children().each(function(){sum+=$(this).outerHeight(true);});
+	return sum;
+}
+
 function toggleSidebar(action){
 	switch (action){
 		case "open":
 			$('#header .sidebar-switch a').addClass("opened");
-			$('#sidebar').addClass("opened").animate({"width": '+=200'});
+			$('#sidebar').addClass("opened").animate({"width": openedSidebar+'px'}, {"queue": false}, openedSidebar*pixelSpeed, "linear");
 			$('#sidebar .sidebar-footer').slideDown();
 			break;
 		case "close":
 			$('#header .sidebar-switch a').removeClass("opened");
-			$('#sidebar').removeClass("opened").animate({"width": '-=200'});
+			$('#sidebar').removeClass("opened").animate({"width": collapsedSidebar+'px'}, {"queue": false}, "linear");
 			$('#sidebar .sidebar-footer').slideUp();
 			$('#sidebar .sidebar-element .collapse.in').slideUp().removeClass("in");
 			$('#sidebar .sidebar-element.active').removeClass('active');
 			break;
 		case "toggle":
 			if ($('#header .sidebar-switch a').hasClass("opened")){
-				$('#sidebar').animate({"width": '-=200'});
+				$('#sidebar').animate({"width": collapsedSidebar+'px'}, {"queue": false}, "linear");
 				$('#sidebar .sidebar-footer').slideUp();
 				$('#sidebar .sidebar-element .collapse.in').slideUp().removeClass("in");
 				$('#sidebar .sidebar-element.active').removeClass('active');
 				//$('#sidebar .sidebar-element .collapse.in').collapse('hide');				
 			} else {
-				$('#sidebar').animate({"width": '+=200'});
+				$('#sidebar').animate({"width": openedSidebar+'px'}, {"queue": false}, "linear");
 				$('#sidebar .sidebar-footer').slideDown();
 			}
 			$('#header .sidebar-switch a').toggleClass("opened");
@@ -30,24 +43,38 @@ function toggleSidebar(action){
 	setPositionContainer();
 }
 function setPositionContainer(){
-	var top = $('#header').outerHeight(true)+$('#message:visible').outerHeight(true);
-	var left = $('#sidebar').hasClass('opened') ? 280 : 80;
-	var width =  $(window).outerWidth(true)-left;
-	$('#mainContainer').css({top: top+"px"});
-	$('#mainContainer').animate({left: left+"px"});
-	$('#mainContainer').animate({width: width+"px"});
+	var topC = $('#header').outerHeight(true)+$('#message:visible').outerHeight(true);
+	var leftC = getLeft();
+	var widthC =  $(window).outerWidth(true)-leftC;
+	$('#mainContainer').css({top: topC+"px"});
+	$('#mainContainer').animate({left: leftC+"px", width: widthC+"px"}, {"queue": false}, "linear");
+	//$('#mainContainer').animate({width: width+"px"}, {"queue": false}, "linear");
 	//$('#mainContainer').css({width: width+"px"});
 }
 function setWidthContainer(){
-	var left = $('#sidebar').hasClass('opened') ? 280 : 80;
-	var width = $(window).outerWidth(true)-left;
-	$('#mainContainer').css({width: width+"px"});
+	var widthC = $(window).outerWidth(true)-getLeft();
+	$('#mainContainer').css({width: widthC+"px"});
+}
+function setWidthFooter(){
+	var widthF = $(window).outerWidth(true)-getLeft();
+	$('#footer').css({width: widthF+"px"});	
 }
 function setHeightContainer(){
-	var heightS = $(window).outerHeight(true)-$('#header').outerHeight(true);
-	var heightC = $(document).outerHeight(true);
-	$('#mainContainer').css("min-height", heightC+"px");
-	$('#sidebar').css("height", heightS+"px");
+	var containerContent = getContentHeight($('#mainContainer'));	
+	var heightC = $(document).outerHeight(true)-$('#header').outerHeight(true);
+	$('#mainContainer').css("height", containerContent+"px");
+	//var topF = heightC-$('#footer').outerHeight(true)-$('#footer').position().top;
+	//console.log("document: "+$(document).outerHeight(true)+"px");
+	//console.log("header: "+$('#header').outerHeight(true)+"px");
+	//console.log("footer: "+$('#footer').outerHeight(true)+"px");
+	//console.log("footer Top: "+$('#footer').position().top+"px");
+	//console.log(topF);
+	//$('#footer').css("top", topF+"px");
+}
+function setHeightSidebar(){
+	var containerContent = getContentHeight($('#mainContainer'));
+	var heightS = $(document).outerHeight(true)-$('#header').outerHeight(true);
+	$('#sidebar').css("height", containerContent+"px");	
 }
 function setWidthRedbar(){
 	var width = $(window).outerWidth(true)-$('#header .sidebar-switch').outerWidth(true)-$('#header .logo').outerWidth(true)-$('#header .online-banking').outerWidth(true);
@@ -78,10 +105,16 @@ $(document).ready(function() {
 	});
 	setWidthRedbar();
 	setWidthContainer();
+	setWidthFooter();
+	setHeightSidebar();
 	setHeightContainer();
+	setPositionContainer();
 	$(window).resize(function(){
-		setWidthContainer();
 		setWidthRedbar();
+		setWidthContainer();
+		setWidthFooter();
+		setHeightSidebar();
 		setHeightContainer();
+		setPositionContainer();
 	});
 });
