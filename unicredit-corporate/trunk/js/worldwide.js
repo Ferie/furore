717dbecfe146dc622ac1be92df9c1f4625f37db1
randreatta <randreatta@70242730-a0bf-483c-8687-6f3b35fb5193>
2015-01-20 15:08:58 +0000

    pagina worldwide

---
+++ unicredit-corporate/trunk/js/worldwide.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$('#worldwideTools .input-daterange').datepicker();
+	$('#worldwidePanelResults .event .footer .attach').click(function(){
+		$(this).toggleClass("collapsed");
+		var swapText = $(this).text();
+		if ($(this).data('memotext')==""){
+			$(this).data('memotext', "Hide attachments");
+		}
+		$(this).text($(this).data('memotext'));
+		$(this).data('memotext', swapText);
+		//$(this).prop("data-memotext", swapText);
+		//$(this).attr("data-memotext", swapText);
+	});
+
+	$('.collapse').on('shown.bs.collapse', function(){
+		headerUBISManager.utils._setHeightContainer();
+		headerUBISManager.utils._setHeightSidebar();
+	});
+	
+	$('.collapse').on('hidden.bs.collapse', function(){
+		headerUBISManager.utils._setHeightContainer();
+		headerUBISManager.utils._setHeightSidebar();
+	});
+});
