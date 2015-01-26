b0d50c220c95e07679bccca8062404370cdee366
randreatta <randreatta@70242730-a0bf-483c-8687-6f3b35fb5193>
2015-01-26 16:51:49 +0000

    read more mobile

--- unicredit-corporate/trunk/js/worldwide.js
+++ unicredit-corporate/trunk/js/worldwide.js
@@ -7,11 +7,81 @@ $(document).ready(function () {
 			$('#accordion .countries').addClass('collapse');
 		}
 	}
+	
+	function readMore() {
+		if (isBreakpoint('xs')) {
+			$('#intro .ourPresenceHiddenOnMobile').hide();
+			$('#intro .readMore').show();
+			$('#intro .readLess').hide();
+
+			$('#intro .readMore').on('click', function (e) {
+				$('#intro .ourPresenceHiddenOnMobile').slideDown();
+				$('#intro .readMore').hide();
+				$('#intro .readLess').show();
+			});
+
+			$('#intro .readLess').on('click', function (e) {
+				$('#intro .ourPresenceHiddenOnMobile').slideUp();
+				$('#intro .readMore').show();
+				$('#intro .readLess').hide();
+			});
+		} else {
+			$('#intro .ourPresenceHiddenOnMobile').show();
+			$('#intro .readMore').hide();
+			$('#intro .readLess').hide();
+		}
+	}
+
+	function tabsHeader() {
+		var partialWidth = 0,
+			tabsNumber = 0,
+			totalPadding,
+			tabPadding;
+
+		$('#tabs .tab').each(function () {
+			var $this = $(this);
+			partialWidth = $this.outerWidth() + partialWidth;
+			tabsNumber++;
+		});
+
+//		if (partialWidth !== $('#tabs .container').width()) {
+			totalPadding = $('#tabs .container').width() - partialWidth;
+
+			tabPadding = (totalPadding / tabsNumber) / 2;
+
+			$('#tabs .tab').each(function () {
+				var $this = $(this);
+				$this.css('padding-left', tabPadding);
+				$this.css('padding-right', tabPadding);
+			});
+//		}
+
+		$('#tabs .tab').on('click', function (e) {
+			e.preventDefault();
+
+			var $this = $(this);
+			$('#tabs .tab').removeClass('active');
+			$this.addClass('active');
+
+			var target = $this.data('tabbody');
+			$('#tabsBody .tabPanel').removeClass('selected');
+			$('#' + target).addClass('selected');
+		});
+	}
 
 	toggleCollapse();
+	readMore();
+	tabsHeader();
+
+	var timer = window.setTimeout(function() {}, 0);
 
 	$(window).resize(function () {
 		toggleCollapse();
+		readMore();
+		window.clearTimeout(timer);
+		timer = window.setTimeout(function() {
+			tabsHeader();
+		}, 1000);
 	});
 
 	$('.collapse').on('shown.bs.collapse', function () {
@@ -45,39 +115,4 @@ $(document).ready(function () {
 			$this.addClass('collapsed');
 		}
 	});
-
-	var tabsHeader = (function () {
-		var partialWidth = 0,
-				tabsNumber = 0,
-				totalPadding,
-				tabPadding;
-
-		$('#tabs .tab').each(function () {
-			var $this = $(this);
-			partialWidth = $this.outerWidth() + partialWidth;
-			tabsNumber++;
-		});
-
-		totalPadding = $('#tabs .container').width() - partialWidth;
-
-		tabPadding = (totalPadding / tabsNumber) / 2;
-
-		$('#tabs .tab').each(function () {
-			var $this = $(this);
-			$this.css('padding-left', tabPadding);
-			$this.css('padding-right', tabPadding);
-		});
-
-		$('#tabs .tab').on('click', function (e) {
-			e.preventDefault();
-
-			var $this = $(this);
-			$('#tabs .tab').removeClass('active');
-			$this.addClass('active');
-
-			var target = $this.data('tabbody');
-			$('#tabsBody .tabPanel').removeClass('selected');
-			$('#' + target).addClass('selected');
-		});
-	})();
 });
