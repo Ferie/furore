baa2cf14e070cf06c2b39da287d8853129237e2c
randreatta <randreatta@70242730-a0bf-483c-8687-6f3b35fb5193>
2015-02-03 17:13:17 +0000

    checkbox e radio specifici per ubis con l'utilizzo del plugin icheckbox

--- unicredit-corporate/trunk/js/form.js
+++ unicredit-corporate/trunk/js/form.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {
 	$('.checkbox input[type="checkbox"], .radio input[type="radio"]').iCheck({
-		checkboxClass: 'icheckbox_polaris',
-		radioClass: 'iradio',
+		checkboxClass: 'icheckbox_ubis',
+		radioClass: 'iradio_ubis',
 		aria: true,
 		labelHover: false,
 		cursor: true,
 		increaseArea: '20%'
 	});
-
-	
 });
