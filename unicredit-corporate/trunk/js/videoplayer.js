0ed822cf8c19a6306aaedc4c43f2590666e4e722
gmacrimasi <gmacrimasi@70242730-a0bf-483c-8687-6f3b35fb5193>
2015-02-12 17:25:52 +0000

    Inserimento modal Player

--- unicredit-corporate/trunk/js/videoplayer.js
+++ unicredit-corporate/trunk/js/videoplayer.js
@@ -1,12 +1,12 @@
+function clickPlay(target){
+	$('.ucg_videoplayer iframe').attr("src", $(this).data("video"));
+	$('.ucg_videoplayer.modal').modal();
+}
+
 $(document).ready(function() {
-	//$('.video iframe').css("display","none").css("height", "0");
-	$(".video a").click(function(){
-		$(this).parent().find('iframe').attr("src", $(this).data("video"));
-		//$(this).parent().find('a, img').fadeOut("slow", function(){
-		//	$(this).parent().find('iframe').css("height", "auto").fadeIn();			
-		//});
-		$(this).parent().find('.modal').modal();
-		//$(this).parent().find('img').fadeOut();
-		
-	});
+//	$("a.video").click(function(){
+//		$(this).parent().find('iframe').attr("src", $(this).data("video"));
+//		$(this).parent().find('.modal').modal();		
+//	});
+	$("a.ucg_videoplayer_starter").click(clickPlay);
 });
