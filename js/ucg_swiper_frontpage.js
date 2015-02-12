465ab7e0d43c42f552f7bd28a48f70610cc3a3d6
gmacrimasi <gmacrimasi@70242730-a0bf-483c-8687-6f3b35fb5193>
2015-02-12 17:25:52 +0000

    Inserimento modal Player

--- js/ucg_swiper_frontpage.js
+++ js/ucg_swiper_frontpage.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
+	$(".ucg_swiper_content .ucg_swiper_text a.video").click(clickPlay);
   var ucgSwiper = $('.ucg_swiper .swiper-container').swiper({
 	    mode:'horizontal',
 	    loop: true,
 	    autoplay: 5000,
-	    createPagination: true,
-	    paginationClickable: true,
-	    pagination: ".ucg_swiper_pagination",
+	    createPagination: false,
 		autoResize: true,
 		resizeReInit: true,
 	    onInit: function(swiper){
@@ -13,8 +12,10 @@ $(document).ready(function() {
 	    onSlideChangeStart: function(swiper){
 	    	$(".ucg_swiper_content .ucg_swiper_text h3").text($(swiper.slides[swiper.activeIndex]).data("title"));
 	    	$(".ucg_swiper_content .ucg_swiper_text h4").text($(swiper.slides[swiper.activeIndex]).data("subtitle"));
-	    	
 	    	$(".ucg_swiper_content .ucg_swiper_text a").text($(swiper.slides[swiper.activeIndex]).data("linklabel")).removeAttr("class").attr("href", $(swiper.slides[swiper.activeIndex]).data("url")).attr("target", $(swiper.slides[swiper.activeIndex]).data("target")).addClass($(swiper.slides[swiper.activeIndex]).data("link"));
+	    	$(".ucg_swiper_content .ucg_swiper_text a").off("click");
+	    	$(".ucg_swiper_content .ucg_swiper_text a").data("video", "");
+	    	$(".ucg_swiper_content .ucg_swiper_text a.video").data("video", $(swiper.slides[swiper.activeIndex]).data("video")).click(clickPlay);
 	    }	
 	  });
   $('.ucg_swiper_arrows .ucg_swiper_arrow.ucg_swiper_arrow_left a').click(function(){
