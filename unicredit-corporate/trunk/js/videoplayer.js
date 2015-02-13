d8937d7360320501909384463cffc4c647ec9b86
randreatta <randreatta@70242730-a0bf-483c-8687-6f3b35fb5193>
2015-02-13 10:45:32 +0000

    aggiornamento componenti

--- unicredit-corporate/trunk/js/videoplayer.js
+++ unicredit-corporate/trunk/js/videoplayer.js
@@ -4,9 +4,17 @@ function clickPlay(target){
 }
 
 $(document).ready(function() {
-//	$("a.video").click(function(){
-//		$(this).parent().find('iframe').attr("src", $(this).data("video"));
-//		$(this).parent().find('.modal').modal();		
-//	});
 	$("a.ucg_videoplayer_starter").click(clickPlay);
+
+	$('.ucg_video img').on('load', function () {
+		var $this = $(this),
+			imageHeight = $this.height(),
+			imageWidth = $this.width();
+			
+		$this.siblings('.ucg_videoplayer_starter').width(imageWidth).height(imageHeight);
+	});
+
+	$('.ucg_videoplayer').on('hide.bs.modal', function () {
+		$('.ucg_videoplayer iframe').attr("src", '');
+	})
 });
