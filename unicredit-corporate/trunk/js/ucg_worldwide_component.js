d86e0dafb9dc786fbc58d3f9a932872e1b09d25e
randreatta <randreatta@70242730-a0bf-483c-8687-6f3b35fb5193>
2015-02-20 11:43:36 +0000

    aggiornamento history

--- unicredit-corporate/trunk/js/ucg_worldwide_component.js
+++ unicredit-corporate/trunk/js/ucg_worldwide_component.js
@@ -23,23 +23,16 @@ $(document).ready(function () {
 		yearsSwiper.swipeNext();
 	});
 
-//	var historySwiper = new Swiper('.ucg_worldwide .history_content .swiper-container', {
-//		spaceBetween: 30,
-//		resizeReInit: true
-//	});
-
 	$('.ucg_worldwide .nations_container .swiper-slide a').on('click', function () {
 		var $this = $(this);
 		if (!$this.parent().hasClass('active')) {
 			$('.ucg_worldwide .nations_container .swiper-slide').removeClass('active');
 			$this.parent().addClass('active');
-//			historySwiper.swipeTo($this.data('slide'), 500, false);
 		}
 	});
 
 	$('#mainContainer').on("animationSidebarCompleted", function () {
 		reinitSwiper(yearsSwiper);
-//		reinitSwiper(historySwiper);
 		sidebarResize();
 	});
 
