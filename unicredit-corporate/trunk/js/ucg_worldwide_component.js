4f4a8e0acd8c8d7b4392b89e1a9fe6727636ef10
randreatta <randreatta@70242730-a0bf-483c-8687-6f3b35fb5193>
2015-02-23 11:34:49 +0000

    worldwide component

--- unicredit-corporate/trunk/js/ucg_worldwide_component.js
+++ unicredit-corporate/trunk/js/ucg_worldwide_component.js
@@ -1,26 +1,54 @@
 $(document).ready(function () {
 	function sidebarResize() {
 		headerUBISManager.utils._setHeightContainers();
+		mainContainerWidth = $('#mainContainer').width();
+
+		if (mainContainerWidth >= 920) {
+			numSlidesPerView = 7;
+			reinitSwiper();
+		} else if (mainContainerWidth >= 860 && mainContainerWidth < 920) {
+			numSlidesPerView = 6;
+			reinitSwiper();
+		} else if (mainContainerWidth >= 820 && mainContainerWidth < 860) {
+			numSlidesPerView = 5;
+			reinitSwiper();
+		} else if (mainContainerWidth >= 720 && mainContainerWidth < 820) {
+			numSlidesPerView = 4;
+			reinitSwiper();
+		} else if (mainContainerWidth >= 560 && mainContainerWidth < 720) {
+			numSlidesPerView = 3;
+			reinitSwiper();
+		} else if (mainContainerWidth < 560) {
+			numSlidesPerView = 2;
+			reinitSwiper();
+		}
 	}
 
-	function reinitSwiper(swiper) {
-		swiper.resizeFix(true);
+	function reinitSwiper() {
+		nationsSwiper = new Swiper('.ucg_worldwide .nations_container .swiper-container', {
+			slidesPerView: numSlidesPerView,
+			mousewheelControl: true,
+			resizeReInit: true,
+			freeMode: true
+		});
+
+		if ($('.ucg_worldwide .swiper-slide').first().width() >= 170) {
+			$('.ucg_worldwide .swiper-slide, .ucg_worldwide .swiper-wrapper').height(22);
+		} else {
+			$('.ucg_worldwide .swiper-slide, .ucg_worldwide .swiper-wrapper').height(44);
+		}
 	}
 
-	var yearsSwiper = new Swiper('.ucg_worldwide .nations_container .swiper-container', {
-		slidesPerView: 5,
-		spaceBetween: 30,
-		mousewheelControl: true,
-		resizeReInit: true,
-		freeMode: true
-	});
+	var mainContainerWidth,
+		numSlidesPerView,
+		nationsSwiper;
 
 	$('.ucg_worldwide .ucg_worldwide_arrow_left a').on('click', function () {
-		yearsSwiper.swipePrev();
+		nationsSwiper.swipePrev();
 	});
 
 	$('.ucg_worldwide .ucg_worldwide_arrow_right a').on('click', function () {
-		yearsSwiper.swipeNext();
+		nationsSwiper.swipeNext();
 	});
 
 	$('.ucg_worldwide .nations_container .swiper-slide a').on('click', function () {
@@ -32,7 +60,6 @@ $(document).ready(function () {
 	});
 
 	$('#mainContainer').on("animationSidebarCompleted", function () {
-		reinitSwiper(yearsSwiper);
 		sidebarResize();
 	});
 
@@ -40,4 +67,14 @@ $(document).ready(function () {
 	window.setTimeout(function () {
 		sidebarResize();
 	}, 1000);
+
+	var timer;
+
+	// functions on resize
+	$(window).resize(function () {
+		window.clearTimeout(timer);
+		timer = window.setTimeout(function () {
+			sidebarResize();
+		}, 400);
+	});
 });
