f15902b9270e6f68380c6f34f6b4216ea8f87c31
Riccardo Andreatta <riccardo.andreatta@myoffersltd.co.uk>
2015-09-23 15:16:18 +0100

    new batch

--- batch/htmlGetter.bat
+++ batch/htmlGetter.bat
@@ -1,7 +1,7 @@
 
 REM Sostituire i propri path
 cd "C:\Program Files\GnuWin32\bin"
-set rootSour=http://localhost:8080/gb-energy-website-v2-main-site/app/furore/
+set rootSour=http://localhost:8080/gb-energy-website-v2-main-site/app/furore/jsp/
 set rootDest=C:\websites\gb-energy-website-v2-main-site\app\furore
 set logDest=C:\websites\gb-energy-website-v2-main-site\app\furore\wget_log.log
 set start=%date:~-4%-%date:~3,2%-%date:~0,2%---%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%
