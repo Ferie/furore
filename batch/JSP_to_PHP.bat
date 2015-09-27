dca6c8c36b7294be45f489ff405b0970597cce90
Riccardo <ferie150@hotmail.com>
2015-09-27 15:40:16 +0100

    corretti i batch

--- batch/JSP_to_PHP.bat
+++ batch/JSP_to_PHP.bat
@@ -1,4 +1,7 @@
 cls
-cd "C:\Informatica\xampp\htdocs\GitHub\furore\jsp"
+ECHO 
+ECHO Inizio copia dei file nella cartella php
 php copy.php
+ECHO 
+ECHO Inizio conversione delle istruzioni JSP in istruzioni PHP
 php conversion.php
